70ea9c3dad4665391a4038e6c085cf964d6f7bfc
hkan <hermann.kan.dev@gmail.com>
2023-02-22 10:23:46 -0500

    deleted package json and reinstall modules

--- server/src/app.js
+++ server/src/app.js
@@ -10,14 +10,11 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-// app.use("v1", api);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
-// app.get('/', (req, res) => {
-//   res.send('Hello change from vs code!')
-// })
+
 
 module.exports = app
